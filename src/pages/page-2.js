f6b468673c450574ee1b166ebdcecaecdf44ee15
Sander van Rijsoort <sander.vanrijsoort@incentro.com>
2019-10-02 16:14:23 +0200

    basic layout + styles

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,16 +1,46 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-
 const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
+    
+
+
+<div class='cv-wrapper'>
+  <div class='row'>
+    <div class='column'>
+        Some Text in Column One
+    </div>
+    <div class='column'>
+    
+      <div class="a4-column">
+        <div class='row'>
+            <div class='column'>
+                <div class="headera4">
+                <h1>[naam]</h1>
+                <h1>[jouw functie hier]</h1>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                </div>
+            </div>
+        </div>
+        <div class='row'>
+            <div class='double-column'>
+                Hi
+            </div>
+            <div class='column'>
+                <div class="orange-column">
+                <h2>info</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+        
+    <div class='column'>
+        Some Text in Column Three
+    </div>
+  </div>
+</div>
+
 )
 
 export default SecondPage
