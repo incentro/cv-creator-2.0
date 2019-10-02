d60863ce892c28fc6460610c8f990d5de3f979fd
Sander van Rijsoort <sander.vanrijsoort@incentro.com>
2019-10-02 17:11:41 +0200

    'v2'

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,14 +1,14 @@
 import React from "react"
 import { Link } from "gatsby"
 
+//Import componenten
+
+
 const SecondPage = () => (
     
-
-
 <div class='cv-wrapper'>
   <div class='row'>
     <div class='column'>
-        Some Text in Column One
     </div>
     <div class='column'>
     
@@ -24,19 +24,43 @@ const SecondPage = () => (
         </div>
         <div class='row'>
             <div class='double-column'>
-                Hi
+                <h1>Werkervaring</h1>
+    
+                    <div class="row">
+                        <div class="column">
+                        <strong>2017 - nu</strong>
+                        <h2>Front-end Developer @ Coop</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                        </div>
+                    </div>    
+    
+                <h1>Opleidingen</h1>
+    
+                        <div class="row">
+                        <div class="column">
+                        <strong>2017 - nu</strong>
+                        <h2>Bedrijfskunde @ Erasmus Universiteit</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                        </div>
+                    </div>  
+
             </div>
             <div class='column'>
                 <div class="orange-column">
-                <h2>info</h2>
-                </div>
+                <h1 className="white">info</h1>
+    
+                <div class="row">
+                    <div class="column">
+                    
+                    </div>
+                </div>  
+            </div>
             </div>
         </div>
     </div>
 </div>
         
     <div class='column'>
-        Some Text in Column Three
     </div>
   </div>
 </div>
