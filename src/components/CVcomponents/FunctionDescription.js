1f2e74ece4b13973412a9d917b87f421db3a98e7
Sander van Rijsoort <sander.vanrijsoort@incentro.com>
2019-10-16 14:28:28 +0200

    Feedback implemented

--- src/components/CVcomponents/FunctionDescription.js
+++ src/components/CVcomponents/FunctionDescription.js
@@ -1,17 +1,53 @@
-import React from 'react';
+import React, { useState } from "react"
+
 //Importing components
 import orangeCircle from "../../images/circle_orange.png"
 
-const FunctionDescription = ( {period, job, description} ) => {
-return  <div className="row">
-            <div className="column timeline">
-              <img src={orangeCircle} alt="orangecircle"/>
-            </div>
-            <div className="column function">
-                <p className="timestamp">{period}</p>
-                <h2>{job}</h2>
-                <p>{description}</p>
-            </div>
-        </div>  
+const FunctionDescription = ( {period, job, description, index, changeItem} ) => {
+  const [isClicked, setIsClicked] = useState(false)
+  const [isTimeClicked, setIsTimeClicked] = useState(false)
+  const [isJobClicked, setIsJobClicked] = useState(false)
+  const [isDescClicked, setIsDescClicked] = useState(false)
+
+  const changeTime = (e) => {
+    changeItem(e, index);
+    setIsTimeClicked(!isTimeClicked);
+  }
+
+  const changeJob = (e) => {
+    changeItem(e, index);
+    setIsJobClicked(!isJobClicked);
+  }
+
+  const changeDescription = (e) => {
+    changeItem(e, index);
+    setIsDescClicked(!isDescClicked);
+  }
+
+return (
+  <div className="row">
+    <div className="column timeline">
+      <img src={orangeCircle} alt="orangecircle"/>
+    </div>
+    <div className="column function">
+
+      {isTimeClicked ?
+        <input type="text" defaultValue={period} id="time" className="timestamp" autoFocus onBlur={changeTime}/>
+        :
+        <p className="timestamp editable" onClick={() => setIsTimeClicked(!isTimeClicked)}>{!!(period) ? period : "Vul een periode in!"}</p>
+      }
+
+      {isJobClicked ?
+        <input type="text" defaultValue={job} className="jobtitle" id="job" autoFocus onBlur={changeJob}/>
+        :
+        <h2 className="editable" onClick={() => setIsJobClicked(!isJobClicked)}>{!!(job) ? job : "Vul een functietitel in!"}</h2>
+      }
+
+      {isDescClicked ?
+        <textarea defaultValue={description} className="description" id="description" autoFocus onBlur={changeDescription}/>
+        :
+        <p className="editable" onClick={() => setIsDescClicked(!isDescClicked)}>{description}</p> }
+    </div>
+  </div>  )
 };
 export default FunctionDescription;
